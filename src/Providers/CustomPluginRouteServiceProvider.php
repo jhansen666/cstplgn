fda852a393d43cf9c84cebfa6ae0d06bb7a9c1c0
jhansen666 <hansen@unidomo.com>
2018-04-23 15:55:56 +0200

    da

--- src/Providers/CustomPluginRouteServiceProvider.php
+++ src/Providers/CustomPluginRouteServiceProvider.php
@@ -10,6 +10,6 @@ class CustomPluginRouteServiceProvider extends RouteServiceProvider
 {
     public function map(Router $router)
     {
-        $router->get('main','CustomPlugin\Controllers\ContentController@sayHello');
+        $router->get('main','CustomPlugin\Controllers\CustomPluginContentController@sayHello');
     }
 }
