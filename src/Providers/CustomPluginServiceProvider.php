d72d78ee6cdaf49aadffca17b6e681966a92d5dc
jhansen666 <hansen@unidomo.com>
2018-04-23 14:40:51 +0200

    go

---
+++ src/Providers/CustomPluginServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace CustomPlugin\Providers;
+ 
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class CustomPluginServiceProvider extends ServiceProvider
+{
+ 
+    /**
+     * Register the service provider.
+     */
+ 
+    public function register()
+    {
+ 
+    }
+}
